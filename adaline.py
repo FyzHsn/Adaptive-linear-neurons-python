7ad7b1e42287aefaf49ff78fadf3b25aacd35bbb
Faiyaz Hasan <faiyaz.hasan1@gmail.com>
2016-07-27 12:50:47 -0400

    Began Adaline algorithms

---
+++ adaline.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 27 12:42:47 2016
+
+Here, we take the perceptron algorithm and introduce the standard square
+error (SSE) cost function to modify it to be the ADALINE algorithm. The
+adaline algorithm is short for Adaptive Linear Neuron algorithm. The
+weights are updated according to the gradient of the cost function since
+it is convex and differentiable.
+
+"""
+import numpy as np
+class Adaline(object):
+    """Adaline classifier.
+    
+    Parameters
+    ----------
+    eta : float
+        Learning rate between 0.0 and 1.0.
+    n_iter : int
+        Number of passes over the training dataset.
+    
+    Attributes
+    ----------
+    w_ : 1d-array
+        Weights after fitting. Underscore after a variable name indicates 
+        that the variable was not created on instantiation of the object.
+    cost_ : list
+        Cost function for each sample batch and weight vector.
+        
+    """
+    def __init__(self, eta=0.01, n_iter=10):
+        self.eta = eta
+        self.n_iter = n_iter
+    
+    def fit(self, X, y):
+        """Fit training data according to the adaline algorithm.
+        
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_samples, n_features]
+            Training dataset, where n_samples is the number of samples
+            and n_features is the number of features.
+        y : {array-like}, shape = [n_samples]
+            Binary classification of dataset.
+                    
+        Returns
+        -------
+        self : object
+        
+        """
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.errors_ = []
+        
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[1] += update
+                errors += int(update != 0)
+            self.errors_.append(errors)
+        return self
+        
+    def net_input(self, X):
+        """Calculate the dot product of the features and the weights. """
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+        
+    def predict(self, X):
+        """Return class label by using the Heaviside activation
+        function. """        
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+
---
+++ adaline_script.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 27 12:43:15 2016
+
+In this script, we test the adaline algorithm.
+"""
+
